370e57dc7002b9b1101934a4e1b7b645ced4320c
forgit <aaaa111@mail.ru>
2014-01-18 14:54:32 +0400

    Modify Euler.hs

--- Euler.hs
+++ Euler.hs
@@ -1,27 +1,16 @@
 f :: Floating a => a -> a -> a
-f x y = y/x - 4/(x*x)
+f x y = sin(x) - cos(y)
 
-g :: Floating a => a -> a -> a
-g x y = x + y
-
-eiler :: (Floating a, Enum a, Integral b) => (a -> a -> a) -> a -> a -> b -> a -> [a]
-eiler f a b n y0 = drop 2 $ yk a b n y0
+eiler :: (Floating a, Enum a, Integral b) => (a -> a -> a) -> a -> a -> b -> a -> [(a,a)]
+eiler f a b n y0 = zip xk (drop 1 $ yk a b n y0)
 	where 
 		yk a b n y0 = 0 : y0 : 
 			zipWith 
-				(\y x -> y + h*(f x y)) 
+				(\y x -> y + h * (f x y)) 
 					(tail $ yk a b n y0) 
-					([a + k*h | k<-[0 .. fromIntegral n]])
+					xk
 
-		h = (b-a)/ fromIntegral n		
+		h = (b-a) / fromIntegral n		
+		xk = [ a + k * h | k <- [0 .. fromIntegral n] ]
 
-
-main = do
-	let
-		a = 1
-		b = 3
-		n = 10
-		y0 = 2
-
-	print $ eiler f a b n y0
-	print $ eiler g 0 0.4 4 1
+main = print $ eiler f 0 1 5 1
