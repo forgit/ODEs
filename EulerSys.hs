245c1746e8ce134b6365b02f9b7aee845a44dedd
forgit <aaaa111@mail.ru>
2014-02-15 19:19:16 +0400

    Add Euler method for a system

--- EulerSys.hs
+++ EulerSys.hs
@@ -1,66 +1,23 @@
-import Control.Applicative
-import Control.Monad
-import qualified Data.Foldable as F
+module EulerSys (eulerSys) where
 
-euler :: (Floating a, Enum a, Integral b) => (a -> a -> a) -> a -> a -> b -> a -> [(a,a)]
-euler f a b n y0 = zip xk (drop 1 $ yk a b n y0)
+import Data.List
+
+f :: Double -> [Double] -> [Double]
+f x ys = [z, 2*x*z/(x*x+1)]
+	where z = ys!!1
+
+exactSol :: (Floating a) => a -> a
+exactSol x = -x**3 + 3*x + 1
+
+
+eulerSys :: (Floating a, Enum a, Integral b) => (a -> [a] -> [a]) -> a -> a -> b -> [a] -> [(a, [a])]
+eulerSys f a b n y0 = take (1 + fromIntegral n) $ yk f a b n y0
 	where 
-		yk a b n y0 = 0 : y0 : 
-			zipWith 
-				(\y x -> y + h * (f x y)) 
-					(tail $ yk a b n y0) 
-					xk
+		yk f a b n y0 = (a, y0) : map (\(x,y)->(x + h, newy x y)) (yk f a b n y0)
 
-		h = (b-a) / fromIntegral n		
-		xk = [ a + k * h | k <- [0 .. fromIntegral n] ]
+		h = (b-a) / fromIntegral n
+		newy x y = zipWith (+) y $ map (*h) (f x y)
 
---g = \x -> fmap (+) $ return x
-
-h = [(\x y->x+y-4),(\x _->x+2)]
-q = [(\x->x+2),(\y->y-1)]
-
-
---f = [(\x _->sin x),(\_ y->sin y)]
-
-yk f a b n y0 = (a,y0) : map (\(x,y) -> (x+h, y + h * (f x y))) (yk f a b n y0)
-	where h = (b-a) / fromIntegral n
-
---yk' f@(ff:fs) a b n y0@(yy:ys) = (a,y0) : map (\(x,y) -> (x+h, map (id) y)) (yk' f a b n y0)
-
---yk' :: (Floating a, Enum a, Integral b) => [(a -> a -> a->a)] -> a -> a -> b -> [a] -> [(a,[a])]
-yk' f a b n y0 = (a,y0) : map (\(x,y) -> (x+h, zipWith (+) (map (*h) (apply f (x:y))) y)) (yk' f a b n y0)
-	where h = (b-a) / fromIntegral n
-
-f :: [Double -> Double -> Double -> Double]
-f = [(\x y1 y2 -> x+y1+y2),(\x y1 y2 -> x-y1-y2)]
-
-apply fs xs = map (\f -> foldl1 f xs) fs
-
-aa fs xs = fs <*> (head xs)
-
-bb [] = []
-bb xs = head xs <*> bb (tail xs)
-
---cc f xs = f <$> 
 
 main = do
---	print $ take 11 $ yk (\ x y -> sin(x) - cos(y)) 0 1 10 1
---	print $ take 11 $ yk' [(\ x y -> sin(x) - cos(y))] 0 1 10 [1]
-	--print $ take 11 $ yk' f 0 1 10 [1,1]
-
-	--print $ apply h [1,2]
-	--print $ apply f [1,2,3]
-	print $ (\x y z-> x+y+z) <$> [2] <*> [3] <*> [4]
-	print $ f <*> [1] <*> [3] <*> [3]
-	print $ [(+)] <*> [1] <*> [2]
-	--print $ F.foldrM (<*>) [[(+)],[1],[2]]
-	print $ F.fold ([[1,2]])
-	print $ F.toList $  [1,2,3]
-	print $ 
-
-
-
-
-
-
-
+	print $ eulerSys f 0 1 5 [1,3]
