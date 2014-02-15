6189ebf83e12a9d1714dda23089418b9474063bc
forgit <aaaa111@mail.ru>
2014-02-15 19:22:03 +0400

    Add RK4 method for systems

--- RungeKuttaSys.hs
+++ RungeKuttaSys.hs
@@ -1,55 +1,28 @@
--- https://sites.google.com/site/srihariramanathan/runge-kuttawithhaskell
--- http://www.schemers.org/Documents/Standards/R5RS/HTML/
+module RungeKuttaSys (rk4Sys) where
+
+import Data.List
+
+f :: Double -> [Double] -> [Double]
+f x ys = [z, 2*x*z/(x*x+1)]
+    where z = ys!!1
+
+exactSol :: (Floating a) => a -> a
+exactSol x = -x**3 + 3*x + 1
 
 
-instance (Num a) => Num [a] where
-    (+) = zipWith (+)
-    negate = fmap negate
-    fromInteger = undefined
-    (*) = undefined
-    abs = undefined
-    signum = undefined
+rk4Sys :: (Floating a, Enum a, Integral b) => (a -> [a] -> [a]) -> a -> a -> b -> [a] -> [(a, [a])]
+rk4Sys f a b n y0 = take (1 + fromIntegral n) $ yk f a b n y0
+    where
+        yk f a b n y0 = (a,y0) : map (\(x,y)->(x + h, newy x y)) (yk f a b n y0)
 
-integrate_system :: (Functor f, Floating a, Num (f a)) =>
-                    (f a -> f a) -> f a -> a -> [f a]
-integrate_system system_derivative initial_state h =
-    let next = runge_kutta4 system_derivative h
-        runge_kutta4 f h y =
-            let
-                (.*) n = fmap (*n)
-                shf = ((1/2).*)
-                k0 = (f y) 
-                k1 = h .* (y + shf k0)
-                k2 = h .* (y + shf k1)
-                k3 = h .* (y + k2)
-            in
-              y + (h/6) .* (k0 + 2 .* k1 + 2 .* k2 + k3)
-    in
-      iterate next initial_state
+        h = (b-a) / fromIntegral n
+        newy x y = zipWith (+) y $ zipWith4 (\_k1 _k2 _k3 _k4 -> 1/6*(_k1 + 2*_k2 + 2*_k3 + _k4)) (k1 x y) (k2 x y) (k3 x y) (k4 x y)
 
-runge_kutta4 f h y = y + (h/6) .* (k0 + 2 .* k1 + 2 .* k2 + k3)
-            where
-                (.*) n = fmap (*n)
-                shf = ((1/2).*)
-                k0 = (f y) 
-                k1 = h .* (y + shf k0)
-                k2 = h .* (y + shf k1)
-                k3 = h .* (y + k2)
-                    
+        k1 x y = map (*h) (f x y)
+        k2 x y = map (*h) (f (x+h/2) (zipWith (+) y $ map (*0.5) $ k1 x y))
+        k3 x y = map (*h) (f (x+h/2) (zipWith (+) y $ map (*0.5) $ k2 x y))
+        k4 x y = map (*h) (f (x+h) (zipWith (+) y $ k3 x y))
 
-rossler a b c (x:y:z:_) = [-(y+z), x+a*y, b + x*z - c*z]
-lorenz s r b (x:y:z:_) = [s * (y - z), r * x - y - x * z, x * y - b * z]
-
---expEq k = integrate_system (\(y:_) -> y) 
-
---f :: (Functor f, Floating a, Num (f a)) => f a -> f a
-f y = y
-
-g x y = sin(x) - cos(y)
 
 main = do
-    print $ 0
-    --print $ integrate_system f [1,2] 0.1
-    print $ runge_kutta4 f 0.1 [1,2,3]
-    --print $ let (.*) n = fmap (*n) in 2.* (f [1])
-    print $ f [1,2]
+    print $ rk4Sys f 0 1 5 [1,3]
